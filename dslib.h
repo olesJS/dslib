5e0cfc92351945fe737c191168c9a3a179a30530
Oleksii Yakymchuk <oles0305@gmail.com>
2025-07-11 20:41:07 +0300

    Added dslib header file

---
+++ dslib.h
@@ -0,0 +1,10 @@
+#ifndef DSLIB_H
+#define DSLIB_H
+
+#include "ds_stack.h"       // STACK library
+#include "ds_queue.h"       // QUEUE library
+#include "ds_dequeue.h"     // DEQUEUE library
+#include "ds_bin_tree.h"    // BINARY SEARCH TREE library
+#include "ds_hash_table.h"  // HASH TABLE library
+
+#endif //DSLIB_H
--- main.c
+++ main.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "ds_stack.h"       // STACK library
-#include "ds_queue.h"       // QUEUE library
-#include "ds_dequeue.h"     // DEQUEUE library
-#include "ds_bin_tree.h"    // BINARY SEARCH TREE library
-#include "ds_hash_table.h"  // HASH TABLE library
+#include "dslib.h"
 
 typedef struct info_part {
     char *name;
